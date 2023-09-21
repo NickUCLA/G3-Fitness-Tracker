33f5e9c9cbdcfce5a867dc782548db4d242cff72
unknown <adrian.garcia.s.1607@gmail.com>
2023-09-20 20:56:48 -0700

    created the structure for the data base as for the user log

--- models/activityLog.js
+++ models/activityLog.js
@@ -1,10 +1,34 @@
 const {Model, DataTypes} = require('sequelize')
 const sequelize = require('../config/connection')
 
-class activityLog extends Model {
-    static associate(models) {
-        activityLog.belongsTo(models.User, {
-          foreignKey: 'userId',
-        });
+class activityLog extends Model {}
+
+activityLog.init (
+    {
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+
+    text: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+
+    user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: "user",   // matches the model name in User
+          key: "id",       // matches the primary key in User
+        },
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "comment",
+    }
 }
-}
+)
