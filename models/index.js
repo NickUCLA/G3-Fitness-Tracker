efaad0f25906ed6423a6b09422e80fa458cc7716
Fabian De La Pena Montero <fdelapenamontero@bootcampspot.com>
2023-09-21 18:58:28 -0700

    Added logic for post and get workouts including handlebars integration

--- models/index.js
+++ models/index.js
@@ -1,8 +1,9 @@
 const User = require("./User");
 const Weight = require("./weight");
+const Workout = require("./workout")
 
 User.hasMany(Weight, {
   foreignKey: "user_id",
 });
 
-module.exports = { User, Weight };
+module.exports = { User, Weight, Workout };
