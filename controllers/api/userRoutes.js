bb1ee6d663831ea6b8743f2bb08660cd3251d8ef
Kenneth Westbrook <kennethwestbrook17@gmail.com>
2023-09-23 13:38:23 -0700

    configured user sign up

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -5,6 +5,21 @@ const userController = require("../userController");
 // Define the route to retrieve user weight data
 router.get("/api/user", userController.getUserWeights);
 
+router.post('/', async (req, res) => {
+  try {
+    const userData = await User.create(req.body);
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+
+      res.status(200).json(userData);
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
 router.post("/login", async (req, res) => {
   try {
     // Find the user who matches the posted e-mail address
